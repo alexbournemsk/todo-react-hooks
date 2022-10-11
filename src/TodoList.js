39b2f6572bfb74fec668235b9d4a7e5e4d7e54bc
alexbournemsk <arbiter1@yandex.ru>
2022-10-11 12:16:20 +0300

    add states

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,7 +4,8 @@ import TodoItem from './TodoItem'
 export default function TodoList({todos}) {
   return (
     <ul>
-      {todos.map(item => <TodoItem key={item.id} {...item} />)}
+      {todos.map(item => <TodoItem key={item.id} {...item} /> )}
+      
     </ul>
   )
 }
